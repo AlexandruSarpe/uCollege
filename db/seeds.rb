984e11635b6d3634b9fd03a4d51755b512669fac
jack19952 <sarpe.1692247@studenti.uniroma1.it>
2018-07-30 21:00:37 +0300

    Adding some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,5 +17,11 @@ Student.create first_name: 'student', last_name: 'user',
 Canteen.create first_name: 'canteen', last_name: 'user',
                username: 'canteen', email: 'test_cant@email.com', password: 'testing'
 
+Book.create title: 'Moby Dick', author: 'Herman Melville', description: 'Adventure', owner_id: Student.where(['email = ?', 'test_stud@email.com']).first.id ,current_owner_id: Student.where(['email = ?', 'test_stud@email.com']).first.id
+
+Book.create title: 'War and Peace', author: 'Lev Tolstoj', description: 'Novel', owner_id: Student.where(['email = ?', 'test_stud@email.com']).first.id ,current_owner_id: Student.where(['email = ?', 'test_stud@email.com']).first.id
+
+Book.create title: '1984', author: 'George Orwell', description: 'Novel', owner_id: Student.where(['email = ?', 'test_stud@email.com']).first.id ,current_owner_id: Student.where(['email = ?', 'test_stud@email.com']).first.id
+
 Token.create access_token: ENV['Drive_at'], refresh_token: ENV['Drive_rt'],
              expires_at: ENV['Drive_exp']
